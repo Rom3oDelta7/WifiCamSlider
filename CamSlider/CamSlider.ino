d2e06e52dd2456335127b501ecd547d3830e7bb1
Rom3oDelta7 <Rom3oDelta7@outlook.com>
2016-08-05 18:05:09 -0700

    Code cleanup before implementing the user interface

--- CamSlider/CamSlider.ino
+++ CamSlider/CamSlider.ino
@@ -23,21 +23,33 @@
 
 /*================================= stepper motor interface ==============================
 
-This code was written for a 17HS24-0644S stepper motor
-<fill in details here>
+This code was written for an OSM 17HS24-0644S stepper motor
+ References:
+	http://www.omc-stepperonline.com/nema-17-60ncm85ozin-064a-bipolar-stepper-motor-17hs240644s-p-19.html (product page)
+	http://www.omc-stepperonline.com/download/pdf/17HS24-0644S.pdf (datasheet [partially in Chinese])
+	http://www.osmtec.com/stepper_motors.htm (OSM stepper motor catalog)
+	
+Current controller: TB6612FNG
+	https://www.sparkfun.com/products/9457
+	
+Future driver support:
+	http://www.electrodragon.com/product/stepstick-stepper-driver-board-a4988-v2/ (Allegro A4988 chopper driver (microstepping))
+	
+	
 */
 
-#define MOTORA1			14
-#define MOTORA2			12
-#define MOTORB1			4
-#define MOTORB2			5	
-#define STANDBY			16
+#define MOTORA1				14						// ESP8266 pins
+#define MOTORA2				12
+#define MOTORB1				4
+#define MOTORB2				5	
+#define STANDBY				16
 
-#define STEPS_PER_REV	200
-#define PULLEY_TEETH		20
-#define BELT_PITCH		2
-#define STEPS_PER_MM		(STEPS_PER_REV / (BELT_PITCH * PULLEY_TEETH))
-#define INCH_TO_MM		25.4
+#define STEPS_PER_REV		200
+#define PULLEY_TEETH			20
+#define BELT_PITCH			2						// 2 MM
+#define STEPS_PER_MM			(STEPS_PER_REV / (BELT_PITCH * PULLEY_TEETH))
+#define INCH_TO_MM			25.4
+#define HS24_MAX_SPEED		592.0					// set calculator at http://www.daycounter.com/Calculators/Stepper-Motor-Calculator.phtml
 
 AccelStepper stepper(AccelStepper::FULL4WIRE, MOTORA1, MOTORA2, MOTORB1, MOTORB2);
 
@@ -52,9 +64,9 @@ typedef enum { CARRIAGE_STOP, CARRIAGE_TRAVEL, CARRIAGE_TRAVEL_REVERSE, PARKED }
 
 volatile	bool				newMove = false;							// true when we need to initiate a new move
 volatile bool				clockwise = true;
-volatile EndstopMode		endstopAction = STOP_HERE;
-volatile CarriageMode	carriageState = PARKED;
-volatile bool				debounce = false;
+volatile EndstopMode		endstopAction = REVERSE;				// action to take when an endstop is hit
+volatile CarriageMode	carriageState = PARKED;					// current state of the carriage (for the motion state machine)
+volatile bool				debounce = false;							// ISR debounce control flag
 volatile unsigned long 	currentTime = 0;							// set in loop() so we don't have to call it in the ISR
 volatile unsigned long	debounceStart = 0;						// start of debounce window
 
@@ -65,7 +77,7 @@ float							targetSpeed = 0.0;						// speed in steps/second
 /*
  endstop ISR (used for both endstops)
  set flags & state to be used in loop()
- we clear the debounce flag after the debounce interval in loop()
+ we clear the debounce flag after the debounce interval expires in loop()
 */
 void endstopHit ( void ) {
 	if ( !debounce ) {
@@ -76,14 +88,14 @@ void endstopHit ( void ) {
 		case STOP_HERE:
 			// stop here
 			carriageState = CARRIAGE_STOP;
-			clockwise = !clockwise;							// next move user initiaites is opposite direction
+			clockwise = !clockwise;								// next move user initiaites is opposite direction
 			break;
 			
 		case REVERSE:
 			// reverse direction
 			carriageState = CARRIAGE_TRAVEL_REVERSE;
 			clockwise = !clockwise;
-			newMove = true;
+			newMove = true;										// execute the same move parameters in the opporsite direction
 			break;
 			
 		case ONE_CYCLE:
@@ -97,6 +109,7 @@ void endstopHit ( void ) {
 		default:
 			break;
 		}
+		// set up debounce window (see loop())
 		debounce = true;
 		debounceStart = currentTime;
 	}
@@ -117,7 +130,7 @@ void setup ( void ) {
 	pinMode(LIMIT_MOTOR, INPUT);								// WeMos internal pullup resistor
 	pinMode(LIMIT_END, INPUT_PULLUP);
 	
-	stepper.setMaxSpeed(592.0);								// max steps/sec - see http://www.daycounter.com/Calculators/Stepper-Motor-Calculator.phtml
+	stepper.setMaxSpeed(HS24_MAX_SPEED);					// max steps/sec - 
 	//stepper.setPinsInverted (false, false, true);		// inverted SYBY pin on Alegro A498
 	//stepper.setEnablePin(STANDBY);							// for the Allegro A498 driver only
 	stepper.disableOutputs();									// don't energize the motors until user initiates movement
@@ -128,29 +141,6 @@ void setup ( void ) {
 	attachInterrupt(digitalPinToInterrupt(LIMIT_END), endstopHit, FALLING);
 }
 
-
-/*
-  simple pin debounce function
-*/
-bool readDebounce ( const uint8_t pin, const uint8_t state = LOW ) {
-	static 	bool 				windowClosed = false;
-	static 	unsigned long 	debounceTime = 0;
-	bool							returnValue = false;
-	
-	if ( windowClosed ) {
-		// see if it is time to stop ignoring fluctuations
-		if ( (millis() - debounceTime) > BOUNCE_DELAY ) {
-			windowClosed = false;
-		}
-	}
-	if ( !windowClosed ) {
-		if ( digitalRead(pin) == state ) {
-			returnValue = true;
-			windowClosed = true;
-		}
-	}
-	return returnValue;
-}
  
 void loop ( void ) {
 	static long	travelStart = 0;
@@ -158,6 +148,7 @@ void loop ( void ) {
 	yield();
 
 #if DEBUG >= 2
+	// manual inputs for debugging - note that motor speed will be significantly slower if debug statements are being output
 	static bool askForInput = true;
 	
 	if ( askForInput ) {
@@ -187,7 +178,7 @@ void loop ( void ) {
 		debounce = false;
 	}
 	
-	// movement state machine
+
 #if DEBUG >= 3
 	static int counter = 0;
 	if ( counter++ > 500 ) {
@@ -196,17 +187,19 @@ void loop ( void ) {
 		counter = 0;
 	}
 #endif
+	// motion state machine - move flag always set outside of this FSM
 	switch ( carriageState ) {
 	case CARRIAGE_TRAVEL:
 		/*
 		 when moving CCW, the position will increment negatively from 0 (CW = positive)
-		 however, distanceToGo in CCW rotation will start at target and INCREASE ==> we need to check the 
-		 absolute value of position rather than relying on distanceToGo() which always increases positively regardless of direction
-		 (since it is subtracting a negative number in CCW rotation)
+		 however, distanceToGo in CCW rotation will start at target and INCREASE (in the negative direction)
+		 ==> we need to check the absolute value of position rather than relying on distanceToGo() which
+		 always increases positively regardless of direction (since it is subtracting a negative number in CCW rotation)
 		*/
 		if ( abs(stepper.currentPosition()) < targetPosition ) {
 			stepper.runSpeed();								// constant speed - no acceleration
 		} else {
+			// target reached - stop the motor (and subsequently park it)
 			carriageState = CARRIAGE_STOP;
 		}
 		break;
@@ -217,17 +210,16 @@ void loop ( void ) {
 			Serial.println(String("*** Traveled ") + String(targetPosition) + String(" steps in ") + String((float)(millis() - travelStart)/1000.0) + String(" sec"));
 #endif
 		stepper.stop();
-		stepper.runSpeedToPosition();
+		stepper.runSpeedToPosition();				// a necessary part of the stop mechanism
 		carriageState = PARKED;
 		stepper.disableOutputs();
 		digitalWrite(STANDBY, LOW);
 		break;
 		
 	case CARRIAGE_TRAVEL_REVERSE:
-		// stop before we initiate the opposite move (flag will be true below)
+		// stop the motion. move flag will be set in the ISR so new (opposite) movement will be initiaited below
 		stepper.stop();
 		stepper.runSpeedToPosition();
-		carriageState = CARRIAGE_TRAVEL;
 		break;
 		
 	case PARKED:
